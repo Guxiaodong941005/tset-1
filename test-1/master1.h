bf7480080fd709d61155f8a37124e316e95e3226
gxd <970768639@qq.com>
2015-09-08 21:08:18 +0800

    add1

---
+++ test-1/master1.h
@@ -0,0 +1,13 @@
+//
+//  master1.h
+//  test-1
+//
+//  Created by gxd on 15/9/8.
+//  Copyright (c) 2015年 GXD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface master1 : NSObject
+
+@end
---
+++ test-1/master1.m
@@ -0,0 +1,13 @@
+//
+//  master1.m
+//  test-1
+//
+//  Created by gxd on 15/9/8.
+//  Copyright (c) 2015年 GXD. All rights reserved.
+//
+
+#import "master1.h"
+
+@implementation master1
+
+@end
