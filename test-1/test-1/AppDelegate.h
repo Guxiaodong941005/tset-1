8de8a34f1902634816d65a71a0a021b82f7cb58c
gxd <970768639@qq.com>
2015-09-08 19:24:57 +0800

    g

---
+++ test-1/test-1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test-1
+//
+//  Created by gxd on 15/9/8.
+//  Copyright (c) 2015年 GXD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
